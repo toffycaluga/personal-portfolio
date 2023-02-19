4539735229b351401ff80383a137453f28893bae
toffycaluga <95626402+toffycaluga@users.noreply.github.com>
2023-02-19 18:20:46 -0300

    cambio iconos redes, y favicon

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -18,8 +18,8 @@ function Footer() {
                     </Col>
                     <Col size={12} sm={6} className="text-center text-sm-end">
                         <div className="social-icon">
-                            <a href="https://github.com/toffycaluga"><img src={navIcon1} alt="Icon" /></a>
-                            <a href="https://www.linkedin.com/in/abraham-lillo-lillo/"><img src={navIcon2} alt="Icon" /></a>
+                            <a href="https://github.com/toffycaluga"><img src={navIcon2} alt="Icon" /></a>
+                            <a href="https://www.linkedin.com/in/abraham-lillo-lillo/"><img src={navIcon1} alt="Icon" /></a>
                             <a href="https://www.instagram.com/toffy_caluga/</div>"><img src={navIcon3} alt="Icon" /></a>
                         </div>
                         <p>Copyright 2023. All Rights Reserved</p>
